69da52ca85a58acd50d49949c77c22fbea52242d
Ny Hasinavalona Randriantsarafara <nyhasinavalonar@gmail.com>
2023-10-19 21:28:41 +0200

    chore: added auth page template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sign-in',
+    loadChildren: () =>
+      import('./modules/authentication/authentication.module').then((m) => m.AuthenticationModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/authentication/authentication.component.spec.ts
+++ src/app/modules/authentication/authentication.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthComponent } from './auth.component';
+import { AuthenticationComponent } from './authentication.component';
 
 describe('AuthComponent', () => {
-  let component: AuthComponent;
-  let fixture: ComponentFixture<AuthComponent>;
+  let component: AuthenticationComponent;
+  let fixture: ComponentFixture<AuthenticationComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AuthComponent],
+      declarations: [AuthenticationComponent],
     });
-    fixture = TestBed.createComponent(AuthComponent);
+    fixture = TestBed.createComponent(AuthenticationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
