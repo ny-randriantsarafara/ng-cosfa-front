17df299a3b06272485f4d892b4f7dfcb5e99dbc8
Ny Hasina Randriantsarafara <nyhasinavalonar@gmail.com>
2023-10-20 14:36:11 +0200

    chore: created search bar component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidenavItemComponent } from './components/sidenav-item/sidenav-item.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [DashboardComponent, SidenavComponent, NavbarComponent, SidenavItemComponent],
+  declarations: [DashboardComponent, SidenavComponent, NavbarComponent, SidenavItemComponent, SearchBarComponent],
   imports: [CommonModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
